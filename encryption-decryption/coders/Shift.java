52746244fee97706eeddfd3f076add4b4b957966
adnanb59 <adnan_bhuiyan@hotmail.com>
2020-01-21 00:50:41 -0500

    Finished Encryption-decryption with updated README + code comments

--- encryption-decryption/coders/Shift.java
+++ encryption-decryption/coders/Shift.java
@@ -6,15 +6,11 @@ public class Shift extends Coder {
     private final Character UPPER_MIN = 'A';
     private final Character UPPER_MAX = 'Z';
 
-    /*
-     * @param c -
-     * @param shift -
-     * @param multiplier -
-     * @return
-     */
     @Override
     public Character evaluate(char c, int shift, int multiplier) {
+        // If character is a letter then have it modified, otherwise leave it as-is
         if (Character.isLetter(c)) {
+            // Check if character is upper or lower case to calculate offset for modification calculations
             char local_min = (c >= 'a') ? LOWER_MIN : UPPER_MIN;
             char local_max = (c >= 'a') ? LOWER_MAX : UPPER_MAX;
             return modify(c - local_min + shift * multiplier, local_min, local_max);
--- encryption-decryption/coders/Unicode.java
+++ encryption-decryption/coders/Unicode.java
@@ -4,12 +4,6 @@ public class Unicode extends Coder {
     private final Character MIN = ' ';
     private final Character MAX = '~';
 
-    /*
-    * @param c -
-    * @param shift -
-    * @param multiplier -
-    * @return
-    */
     @Override
     public Character evaluate(char c, int shift, int multiplier) {
         return modify(c - MIN + shift * multiplier, MIN, MAX);
